ad2e57fc8d7c5265f138e48b94a0178542b6c7e4
Ginja <virginija.meilune@gmail.com>
2022-08-31 18:53:05 +1000

    Adding validation functionality scripts

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Testing")
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");4
+
+let isValid = false;
+function validateForm() {
+    //Using Constraint API
+    isValid = form.checkValidity();
+    //Style main message for an error
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "#A62F03";
+    messageContainer.style.borderColor = "#A62F03";
+}
+
+function processFormData(e) {
+    e.preventDefault();
+    //Validate Form
+    validateForm();
+}
+
+//Event Listener
+form.addEventListener('submit', processFormData);
